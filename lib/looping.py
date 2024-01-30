4efda852e994fb3da412c69e6cb3670242d1162b
Yisel Cintora Vega <yiselcintoravega@gmail.com>
2024-01-30 18:52:59 -0500

    completed lab

--- lib/looping.py
+++ lib/looping.py
@@ -1,13 +1,31 @@
 #!/usr/bin/env python3
 
 def happy_new_year():
-    # code goes here!
-    pass
-
+    i = 10
+    while i > 0 :
+       print(i)
+       i -= 1
+    if i == 0:
+        print("Happy New Year!")
+ 
 def square_integers(int_list):
-    # code goes here!
-    pass
+    squared_numbers = [num * num for num in int_list]
+    return squared_numbers 
+    
 
 def fizzbuzz():
-    # code goes here!
-    pass
+    i = 1
+    while i <=100 :
+         if i%3 == 0 and i%5==0:
+            print ("FizzBuzz")
+            i += 1
+         elif i%5 == 0 :
+            print ("Buzz")
+            i += 1
+         elif i%3==0:
+            print ("Fizz")
+            i += 1
+         else:
+            print( i)     
+            i += 1
+    
